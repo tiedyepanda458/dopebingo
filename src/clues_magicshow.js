8b739469643ddfe6e8a705a0b079307d0879c006
doubleaykay <the.a.cars@gmail.com>
2020-04-16 21:31:03 -0400

    dan

--- src/clues_magicshow.js
+++ src/clues_magicshow.js
@@ -1,5 +1,10 @@
 const magic_clues = [
-    
+    "Shake change",
+    "Switching decks",
+    "Having only one card faceup or vice versa",
+    "The word \"watch\"",
+    "Having an audience member sign the card",
+    "A card pulled from somewhere miraculous, but you're pretty sure it's just a duplicate"
 ];
 
 export default magic_clues
