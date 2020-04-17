1777f9ba54b1aba19376bb9bc14cca5423d2f155
doubleaykay <the.a.cars@gmail.com>
2020-04-17 11:44:09 -0400

    Update clues_magicshow.js

--- src/clues_magicshow.js
+++ src/clues_magicshow.js
@@ -4,7 +4,11 @@ const magic_clues = [
     "Having only one card faceup or vice versa",
     "The word \"watch\"",
     "Having an audience member sign the card",
-    "A card pulled from somewhere miraculous, but you're pretty sure it's just a duplicate"
+    "A card pulled from somewhere miraculous, but you're pretty sure it's just a duplicate",
+    "Rubiks cube",
+    "String / rope trick",
+    "Here then there",
+    "Double lift"
 ];
 
 export default magic_clues
